d8dec5347afd255c745d9fa2a94e6e1eeeeb741e
Rohit Pandit <rohit0311@outlook.com>
2021-08-11 12:57:38 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
-import { Switch, Route, useLocation } from 'react-router-dom';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import People from './pages/People';
-import Post from './pages/Post';
-import Login from './pages/Login';
-import PageNotFound from './pages/PageNotFound';
-import Navbar from './components/Navbar';
-import ModalViewPost from './components/ModalViewPost';
-import { useEffect } from 'react';
+import { Switch, Route, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import People from './pages/People'
+import Post from './pages/Post'
+import Login from './pages/Login'
+import PageNotFound from './pages/PageNotFound'
+import Navbar from './components/Navbar'
 
 function App() {
-    let location = useLocation();
-    let background = location.state && location.state.background;
-    console.log('location', location);
-    console.log('background', background);
+  return (
+    <>
+      <Navbar />
 
-    return (
-        <>
-            <Navbar />
-            <hr />
-            <Switch location={background || location}>
-                <Route exact path='/' component={Home} />
-                <Route path='/profile/:id?' component={Profile} />
-                <Route path='/people' component={People} />
-                <Route path='/Login' component={Login} />
-                <Route path='/post/:id' component={Post} />
-                <Route path='' component={PageNotFound} />
-            </Switch>
-
-            {background && <Route path='/post/:id' component={ModalViewPost} />}
-        </>
-    );
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/profile/:id?" component={Profile} />
+        <Route path="/people" component={People} />
+        <Route path="/Login" component={Login} />
+        <Route path="/post/:id" component={Post} />
+        <Route path="" component={PageNotFound} />
+      </Switch>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import { NavLink, Router } from 'react-router-dom';
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
-    return (
-        <div>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/profile'>Profile</NavLink>
-            <NavLink to='/people'>People</NavLink>
+  return (
+    <nav className="navbar navbar-expand navbar-light bg-light shadow-sm ">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Social
+        </a>
+        <div className="m-auto">
+          <ul className="navbar-nav ">
+            <li className="nav-item mx-5">
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li className="nav-item mx-5">
+              <NavLink to="/profile">Profile</NavLink>
+            </li>
+            <li className="nav-item mx-5">
+              <NavLink to="/people">People</NavLink>
+            </li>
+          </ul>
         </div>
-    );
-};
+        <button className="btn btn-primary">Log Out</button>
+      </div>
+    </nav>
+  )
+}
 
-export default Navbar;
+export default Navbar
